3a5bdfe079752c62ffd6b3d3857b1d341a68e06a
Amr Hafez <amrhassan98@hotmail.com>
2020-07-20 02:06:36 +0200

    add web api function'

--- website/app.js
+++ website/app.js
@@ -6,3 +6,14 @@ const key = '583a1fb85e7163a153b90ed55f4e4c03';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+// Function to run API program
+
+
+// Function GET API data
+
+// Function POST API data
+
+//Function to UpdateUI
