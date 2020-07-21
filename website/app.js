494e8a08a1183d9df88247044eb1f53596209f7c
Amr Hafez <amrhassan98@hotmail.com>
2020-07-21 14:17:35 +0200

    additional app.js functionality

--- website/app.js
+++ website/app.js
@@ -11,8 +11,42 @@ document.getElementById('generate').addEventListener('click', performAction);
 
 // Function to run API program
 
+function performAction() {
+
+  const zipCode = document.getElementById('zip').value;
+  const feeling = document.getElementById('feelings').value;
+  console.log(zipCode)
+  console.log(feeling)
+
+  // function to get inserted API Data
+  getData(baseURL, zipCode, key)
+//   .then((data))=> {
+//     const req = {
+//
+//     }
+//   }
+//
+}
+
 
 // Function GET API data
+
+const getData = async (baseURL, zipCode, key)=> {
+
+  let currURL = (baseURL + zipCode + key);
+  console.log(currURL)
+  const response = await fetch(currURL)
+  console.log(response)
+
+  try{
+    const parse = await response.json();
+    console.log(parse)
+    return parse
+  }
+  catch(error) {
+      console.log('Error receiving API response')
+  }
+};
 
 // Function POST API data
 
